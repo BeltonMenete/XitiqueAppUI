0e0d5b8f1aa0a31b94471abf09e35ecc059d9f16
@Menete <beltonmenete@hotmail.com>
2026-05-19 21:04:41 +0200

    feat(dependecies): add tanstack query+router

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,22 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [
-      react(),
-      babel({ presets: [reactCompilerPreset()] }),
-      tailwindcss(),
-   ],
-   server: {
-      port: 4000,
-   },
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  server: {
+    port: 4000,
+  },
 });
+function tanstackRouter(arg0: {
+  target: string;
+  autoCodeSplitting: boolean;
+}): import("vite").PluginOption {
+  throw new Error("Function not implemented.");
+}
