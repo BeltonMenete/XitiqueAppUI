77a80ed588f12acd1e8f079f591aa68080cf5a91
@Menete <beltonmenete@hotmail.com>
2026-05-21 10:18:22 +0200

    fix(naming): rename file to avoid compilation errors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ function Home() {
       <p className="mt-4 text-lg">
         <Link
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          to="/login"
+          to="/Login"
         >
           Entrar
         </Link>
