a0b63d8c43f80f299615b4068a334dc474549189
@Menete <beltonmenete@hotmail.com>
2026-06-22 21:02:32 +0200

    style(routes): step-2 return statement

--- src/routes/organization/_auth/step-2.tsx
+++ src/routes/organization/_auth/step-2.tsx
@@ -1,9 +1,13 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/organization/_auth/step-2')({
     component: StepTwo,
 });
 
 function StepTwo() {
-    <><h1>Step II am coming</h1></>
+    return (
+        <>
+            <h1>Step II am coming</h1>
+        </>
+    );
 }
